d07226ec25263c8c0141360b046844fed088d647
Muhammad Zohaib <syedzohaib7658@gmail.com>
2023-03-14 00:44:14 +0500

    html updated

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
  //  var num = +prompt("what is your fav name?", 5);
   // if (num % 2 === 0) {
  //   console.log("This is an even number");
-   } //else{
+ //else{
   //  console.log("This is an odd");
-   }
+ // var num = +prompt("what is your fav number?");
+ //if(num % 2 === 0 && num % 4 === 0){
+  //  console.log("Both 2 & 4");
+// }else if(num % 2 === 0 ){ 
+ //  console.log("Only 3");
+// }else if (num % 4 === 0) {
+ //  console.log("Only 4");
+// }else if(num % 3 === 0){
+ //  console.log("This is an odd");
+// }
+ 
